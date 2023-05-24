878dda4df9adcb7e3acb0fe631783fe9aab3f545
Jecy2305 <Edu_Cordova1@hotmail.com>
2023-05-24 01:48:47 -0500

    Proyecto v3

--- ProyectoGameCenter/FrmMantenedorPago.Designer.cs
+++ ProyectoGameCenter/FrmMantenedorPago.Designer.cs
@@ -132,7 +132,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.ClientSize = new System.Drawing.Size(702, 410);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
