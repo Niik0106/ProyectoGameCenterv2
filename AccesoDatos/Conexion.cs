26f32578e3aaf05d950b90ed461f038dd666fc30
DiegoSR <diego.sr24@hotmail.com>
2023-06-18 13:10:09 -0500

    Generando Conexion

--- AccesoDatos/Conexion.cs
+++ AccesoDatos/Conexion.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+<<<<<<< HEAD
 using System.Data;
+=======
+>>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,16 +13,34 @@ namespace AccesoDatos
 {
     internal class Conexion
     {
+<<<<<<< HEAD
+=======
+
+        //patron de Diseño Singleton
+>>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
         private static readonly Conexion _instancia = new Conexion();
         public static Conexion Instancia
         {
             get { return Conexion._instancia; }
         }
+<<<<<<< HEAD
         public SqlConnection Conectar()
         {
             SqlConnection cn = new SqlConnection();
             cn.ConnectionString = "Data Source=.; Initial Catalog = DBMoanso; Integrated Security=true";
             return cn;
         }
+=======
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=; Initial Catalog = MOANSO_GAMECENTER;" +//"User ID=sa; Password=123";
+                                "Integrated Security=true";
+
+            return cn;
+        }
+
+>>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
     }
 }
