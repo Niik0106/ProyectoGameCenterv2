3778346dbf0ac6f9186fde57337b7eb569ba72b8
DiegoSR <diego.sr24@hotmail.com>
2023-06-18 13:06:11 -0500

    A

---
+++ AccesoDatos/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    internal class Conexion
+    {
+        private static readonly Conexion _instancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get { return Conexion._instancia; }
+        }
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=.; Initial Catalog = DBMoanso; Integrated Security=true";
+            return cn;
+        }
+    }
+}
