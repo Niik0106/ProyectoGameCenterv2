fbfd8520749fda38a7a3e8dc6d692c07b82dd82d
DiegoSR <diego.sr24@hotmail.com>
2023-06-18 13:37:26 -0500

    CREANDO CAPA CATEGORIA,ENTIDAD Y LOGICA PARA CATERGORIA Y PRODUCTO

--- AccesoDatos/Conexion.cs
+++ AccesoDatos/Conexion.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-<<<<<<< HEAD
 using System.Data;
-=======
->>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
-=======
->>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -16,32 +10,11 @@ namespace AccesoDatos
 {
     internal class Conexion
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-        //patron de Diseño Singleton
->>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
-=======
-
-        //patron de Diseño Singleton
->>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
         private static readonly Conexion _instancia = new Conexion();
         public static Conexion Instancia
         {
             get { return Conexion._instancia; }
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public SqlConnection Conectar()
-        {
-            SqlConnection cn = new SqlConnection();
-            cn.ConnectionString = "Data Source=.; Initial Catalog = DBMoanso; Integrated Security=true";
-            return cn;
-        }
-=======
-=======
->>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
 
         public SqlConnection Conectar()
         {
@@ -51,10 +24,5 @@ namespace AccesoDatos
 
             return cn;
         }
-
-<<<<<<< HEAD
->>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
-=======
->>>>>>> 43f4fd24d0f640784a674bfba32e8930df4cf0f4
     }
 }
---
+++ AccesoDatos/datCategoria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class datCategoria
+    {
+        private static readonly datCategoria _instancia = new datCategoria();
+        
+        public static datCategoria Instancia
+        {
+            get { return datCategoria._instancia; }
+        }
+
+        public List<entCategoria> ListarCiudad()
+        {
+
+        }
+
+        public Boolean InsertarCiudad(entCategoria Cat)
+        {
+           
+        }
+
+        public Boolean EditarCiudad(entCategoria Cat)
+        {
+
+        }
+
+        public Boolean DeshabilitarCiudad(entCategoria Cat)
+        {
+
+        }
+    }
+}
