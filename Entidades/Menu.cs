d64b9cbd32c154673fce28f39dfbc67d5504fbad
Malleck <malleck.1206@gmail.com>
2023-09-23 13:48:04 -0500

    Intento de Roles

---
+++ Entidades/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Menu
+    {
+        public string Nombre { get; set; }
+        public List<SubMenu> ListaSubMenu { get; set; }
+    }
+}
